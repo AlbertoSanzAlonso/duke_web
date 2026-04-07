94c92199e15407ea0440b27454e7094dd55cd653
AlbertoSanzAlonso <sanz_alonso84@hotmail.com>
2026-04-07 13:52:39 +0200

    Cambiado email de envío a hola@dukeburger-sj.com

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -211,6 +211,6 @@ EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.smtp.Ema
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.dondominio.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='contacto@dukeburger-sj.com')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='hola@dukeburger-sj.com')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='') 
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='Duke Burger <contacto@dukeburger-sj.com>')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='Duke Burger <hola@dukeburger-sj.com>')
